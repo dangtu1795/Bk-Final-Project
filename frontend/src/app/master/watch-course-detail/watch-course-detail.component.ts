41034c129bc7542fbb2ed136fd4a58ad312380e4
William Cao <williamcao.cmu@gmail.com>
2018-03-30 15:45:34 +0700

    ui for master

---
+++ frontend/src/app/master/watch-course-detail/watch-course-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-watch-course-detail',
+  templateUrl: './watch-course-detail.component.html',
+  styleUrls: ['./watch-course-detail.component.css']
+})
+export class WatchCourseDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
